3cb7f56b271fc52aa9eb739b3cfd4664eaf941b7
Ubuntu <ubuntu@ip-172-31-10-123.ap-south-1.compute.internal>
2026-09-21 11:50:00 +0000

    Configure S3 backend for Terraform state

--- provider.tf
+++ provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket       = "raja-full-stack-terraform-state-2026"
+    key          = "full-stack/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
